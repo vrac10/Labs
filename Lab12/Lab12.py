d6eff4ef511884841189c399bb82d31b2b3d4309
vrac10 <vrachit106@gmail.com>
2022-12-20 10:00:27 +0530

    Few changes

--- Lab12/Lab12.py
+++ Lab12/Lab12.py
@@ -16,46 +16,46 @@
 """
 
 # 1-A
-# a = lambda x,y : x[0] == y
-# word = input("Enter the word: ")
-# letter = input("Enter the letter to be checked: ")
-# print(a(word,letter))
+a = lambda x,y : x[0] == y
+word = input("Enter the word: ")
+letter = input("Enter the letter to be checked: ")
+print(a(word,letter))
 
 # 1-B
-# import datetime
-# date = datetime.datetime.today().date()
-# day = lambda y : y.month
-# month = lambda z : z.day
-# year = lambda a : a.year
-# print(day(date),month(date),year(date))
+import datetime
+date = datetime.datetime.today().date()
+day = lambda y : y.month
+month = lambda z : z.day
+year = lambda a : a.year
+print(day(date),month(date),year(date))
 
 # 2-A
-# a = ['8aabced', 'abcde', '1ade', 'abdbiad']
-# digitremove = list(filter(lambda x : x[0].isalpha(), a))
-# print(digitremove)
+a = ['8aabced', 'abcde', '1ade', 'abdbiad']
+digitremove = list(filter(lambda x : x[0].isalpha(), a))
+print(digitremove)
 
 # 2-B
-# z = list(filter(lambda x : x % 2 == 0, range(1,20)))
-# a = lambda x : max(x)
-# print(a(z))
+z = list(filter(lambda x : x % 2 == 0, range(1,20)))
+a = lambda x : max(x)
+print(a(z))
  
 # 2-C
-# a = [(2,1),(5,2),(27,6),(69,13),(55,23),(24,12)]
-# x = list(filter(lambda x : True if x[0] % x[1] == 0 else False,a))
-# print(x)
+a = [(2,1),(5,2),(27,6),(69,13),(55,23),(24,12)]
+x = list(filter(lambda x : True if x[0] % x[1] == 0 else False,a))
+print(x)
 
 # 3-A
-# def myreduce(func,iterable):
-#     element = iterable[0]
-#     for i in iterable:
-#         element = func(element,i)
-#     return element
-# a = myreduce(lambda x,y : x if x > y else y, [1,7,12,5,3,24])
-# print(a)
+def myreduce(func,iterable):
+    element = iterable[0]
+    for i in iterable:
+        element = func(element,i)
+    return element
+a = myreduce(lambda x,y : x if x > y else y, [1,7,12,5,3,24])
+print(a)
 
 # 3-B
-# b = myreduce(lambda x,y : x+y, [1,7,12,5,3,24])
-# print(b)
+b = myreduce(lambda x,y : x+y, [1,7,12,5,3,24])
+print(b)
 
     
     
